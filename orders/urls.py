4dce433cc3817c23ef650bd47cb7dd07d8fed4e8
Gamover <lesha543@bk.ru>
2020-11-28 15:34:15 +0300

    realese_v1

---
+++ orders/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path, include
+from . import views
+
+urlpatterns = [
+
+    path('basket_adding/', views.basket_adding, name='basket_adding'),
+    path('checkout/', views.checkout, name='checkout'),
+
+]
